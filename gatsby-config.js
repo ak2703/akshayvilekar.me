27f7227a9c27c2a6b7dc48ef217fb7a813f792c5
akshay <contact@akshayvilekar.me>
2023-06-09 20:24:38 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `akshayvilekar.me`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
